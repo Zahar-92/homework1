17d6e9bb8d73895bc6884ea06a3c24c03977778c
Zahar-92 <danilenkoza@gmail.com>
2023-01-23 17:58:28 +0200

    Домашняя работа №1

---
+++ src/Two.java
@@ -0,0 +1,147 @@
+public class Two {
+    private boolean conditionTwo;
+    private byte byteTwo;
+    private short shortTwo;
+    private int intTwo;
+    private long longTwo;
+    private char chTwo;
+    private float flTwo;
+    private double dblTwo;
+
+    private Boolean condition2Two;
+    private Byte byte2Two;
+    private Short short2Two;
+    private Integer int2Two;
+    private Long long2Two;
+    private Character ch2Two;
+    private Float fl2Two;
+    private Double dbl2Two;
+
+    public boolean isConditionTwo() {
+        return conditionTwo;
+    }
+
+    public void setConditionTwo(boolean conditionTwo) {
+        this.conditionTwo = conditionTwo;
+    }
+
+    public byte getByteTwo() {
+        return byteTwo;
+    }
+
+    public void setByteTwo(byte byteTwo) {
+        this.byteTwo = byteTwo;
+    }
+
+    public short getShortTwo() {
+        return shortTwo;
+    }
+
+    public void setShortTwo(short shortTwo) {
+        this.shortTwo = shortTwo;
+    }
+
+    public int getIntTwo() {
+        return intTwo;
+    }
+
+    public void setIntTwo(int intTwo) {
+        this.intTwo = intTwo;
+    }
+
+    public long getLongTwo() {
+        return longTwo;
+    }
+
+    public void setLongTwo(long longTwo) {
+        this.longTwo = longTwo;
+    }
+
+    public char getChTwo() {
+        return chTwo;
+    }
+
+    public void setChTwo(char chTwo) {
+        this.chTwo = chTwo;
+    }
+
+    public float getFlTwo() {
+        return flTwo;
+    }
+
+    public void setFlTwo(float flTwo) {
+        this.flTwo = flTwo;
+    }
+
+    public double getDblTwo() {
+        return dblTwo;
+    }
+
+    public void setDblTwo(double dblTwo) {
+        this.dblTwo = dblTwo;
+    }
+
+    public Boolean getCondition2Two() {
+        return condition2Two;
+    }
+
+    public void setCondition2Two(Boolean condition2Two) {
+        this.condition2Two = condition2Two;
+    }
+
+    public Byte getByte2Two() {
+        return byte2Two;
+    }
+
+    public void setByte2Two(Byte byte2Two) {
+        this.byte2Two = byte2Two;
+    }
+
+    public Short getShort2Two() {
+        return short2Two;
+    }
+
+    public void setShort2Two(Short short2Two) {
+        this.short2Two = short2Two;
+    }
+
+    public Integer getInt2Two() {
+        return int2Two;
+    }
+
+    public void setInt2Two(Integer int2Two) {
+        this.int2Two = int2Two;
+    }
+
+    public Long getLong2Two() {
+        return long2Two;
+    }
+
+    public void setLong2Two(Long long2Two) {
+        this.long2Two = long2Two;
+    }
+
+    public Character getCh2Two() {
+        return ch2Two;
+    }
+
+    public void setCh2Two(Character ch2Two) {
+        this.ch2Two = ch2Two;
+    }
+
+    public Float getFl2Two() {
+        return fl2Two;
+    }
+
+    public void setFl2Two(Float fl2Two) {
+        this.fl2Two = fl2Two;
+    }
+
+    public Double getDbl2Two() {
+        return dbl2Two;
+    }
+
+    public void setDbl2Two(Double dbl2Two) {
+        this.dbl2Two = dbl2Two;
+    }
+}
